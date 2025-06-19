83542e2a054e0e24df0718e1fab7d6a6a2b1a3e0
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2025-06-19 12:25:25 +0200

    Add header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
@@ -7,20 +6,23 @@ import SaveUserDatasPage from "./Pages/SaveUserDatasPage";
 import UsersPage from "./Pages/UsersPage";
 import AdminPage from "./Pages/AdminPage";
 import ConnectionPage from "./Pages/ConnectionPage";
+import Header from "./Component/Header";
 
 function App({ disableRouter }) {
   const Wrapper = disableRouter ? React.Fragment : ({ children }) => (
     <Router future={{ v7_startTransition: true }}>{children}</Router>
   );
+  
   return (
     <Wrapper>
       <div className="App">
-          <Routes>
-            <Route exact path="/" element={<SaveUserDatasPage />}></Route>
-            <Route exact path="/listUsers" element={<UsersPage />}></Route>
-            <Route exact path="/admin" element={<AdminPage />}></Route>
-            <Route exact path="/admin/se-connecter" element={<ConnectionPage />}></Route>
-          </Routes>
+        {!disableRouter && <Header />}
+        <Routes>
+          <Route exact path="/" element={<SaveUserDatasPage />}></Route>
+          <Route exact path="/listUsers" element={<UsersPage />}></Route>
+          <Route exact path="/admin" element={<AdminPage />}></Route>
+          <Route exact path="/admin/se-connecter" element={<ConnectionPage />}></Route>
+        </Routes>
       </div>
     </Wrapper>
   );
---
+++ my-app/src/Component/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <AppBar position="static" sx={{ marginBottom: '20px', backgroundColor: '#c01515' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          CICD Application
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button 
+            color="inherit" 
+            component={RouterLink} 
+            to="/"
+          >
+            Accueil
+          </Button>
+          <Button 
+            color="inherit" 
+            component={RouterLink} 
+            to="/listUsers"
+          >
+            Liste Utilisateurs
+          </Button>
+          <Button 
+            color="inherit" 
+            component={RouterLink} 
+            to="/admin"
+          >
+            Admin
+          </Button>
+          <Button 
+            color="inherit" 
+            component={RouterLink} 
+            to="/admin/se-connecter"
+          >
+            Connexion
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
